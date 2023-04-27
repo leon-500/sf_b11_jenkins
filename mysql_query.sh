3d2b882ea0f69999b26a76f807f37b3caee49b10
leon-500 <126183173+leon-500@users.noreply.github.com>
2023-04-27 10:43:00 +0500

    files

---
+++ mysql_query.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Example
+DATABASE_NAME=Rfam
+TABLE_NAME=genome
+mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 <<SQL_QUERY
+USE $DATABASE_NAME;
+SELECT * FROM $TABLE_NAME LIMIT 10;
+SELECT COUNT(*) FROM $TABLE_NAME;
+SQL_QUERY
